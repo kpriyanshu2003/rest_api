2b7598c1489ab601b0992b513eff7d2caa43b54e
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-05-17 02:22:27 +0530

    Fixed: Sometimes data was parsed as int

--- src/datentime.js
+++ src/datentime.js
@@ -6,9 +6,9 @@ export function time(tz) {
     );
   else dt = new Date(new Date().toLocaleString("en-US", { timeZone: tz }));
 
-  let h = dt.getHours() < 10 ? "0" + dt.getHours() : dt.getHours();
-  let m = dt.getMinutes() < 10 ? "0" + dt.getMinutes() : dt.getMinutes();
-  let s = dt.getSeconds() < 10 ? "0" + dt.getSeconds() : dt.getSeconds();
+  let h = dt.getHours() < 10 ? `0${dt.getHours()}` : `${dt.getHours()}`;
+  let m = dt.getMinutes() < 10 ? `0${dt.getMinutes()}` : `${dt.getMinutes()}`;
+  let s = dt.getSeconds() < 10 ? `0${dt.getSeconds()}` : `${dt.getSeconds()}`;
   let t = `${h}:${m}:${s}`;
 
   let json = JSON.stringify({ time: t, hours: h, minutes: m, seconds: s });
@@ -24,9 +24,9 @@ export function date(tz) {
     );
   else dt = new Date(new Date().toLocaleString("en-US", { timeZone: tz }));
 
-  let d = dt.getDate() < 10 ? "0" + dt.getDate() : dt.getDate();
-  let m = dt.getMonth() < 10 ? "0" + dt.getMonth() : dt.getMonth();
-  let y = dt.getFullYear() < 10 ? "0" + dt.getFullYear() : dt.getFullYear();
+  let d = dt.getDate() < 10 ? `0${dt.getDate()}` : `${dt.getDate()}`;
+  let m = dt.getMonth() < 10 ? `0${dt.getMonth()}` : `${dt.getMonth()}`;
+  let y = dt.getFullYear() < 10 ? `0${dt.getYear()}` : `${dt.getYear()}`;
   let t = `${d}/${m}/${y}`;
 
   let json = JSON.stringify({ date: t, days: d, month: m, year: y });
