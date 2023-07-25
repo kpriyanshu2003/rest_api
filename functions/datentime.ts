b2b8188fe04ef6fb6322ab87509a139bbfaacb24
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-07-25 13:18:10 +0530

    Refector Functions

--- functions/datentime.ts
+++ functions/datentime.ts
@@ -1,6 +1,4 @@
-interface TimeZone {
-  zone: string;
-}
+import { Request, Response } from "express";
 
 function isValidTimeZone(tz: string): boolean {
   try {
@@ -18,65 +16,35 @@ function isValidTimeZone(tz: string): boolean {
   }
 }
 
-function time(tz: TimeZone):
-  | {
-      time: string;
-      hours: string;
-      minutes: string;
-      seconds: string;
-      timezone: string;
-    }
-  | { message: string } {
-  if (isValidTimeZone(tz.zone)) {
-    const dt = new Date(
-      new Date().toLocaleString("en-US", { timeZone: tz.zone })
-    );
+function time(req: Request, res: Response) {
+  let tz = req.query.zone as string;
+  if (isValidTimeZone(tz)) {
+    const dt = new Date(new Date().toLocaleString("en-US", { timeZone: tz }));
     const h = dt.getHours() < 10 ? `0${dt.getHours()}` : `${dt.getHours()}`;
     const m =
       dt.getMinutes() < 10 ? `0${dt.getMinutes()}` : `${dt.getMinutes()}`;
     const s =
       dt.getSeconds() < 10 ? `0${dt.getSeconds()}` : `${dt.getSeconds()}`;
     const t = `${h}:${m}:${s}`;
-
-    return {
-      time: t,
-      hours: h,
-      minutes: m,
-      seconds: s,
-      timezone: tz.zone,
-    };
+    res
+      .status(200)
+      .json({ time: t, hours: h, minutes: m, seconds: s, timezone: tz });
   }
-  return { message: "Invalid timezone" };
+  res.status(400).json({ message: "Invalid Timezone" });
 }
 
-function date(tz: TimeZone):
-  | {
-      date: string;
-      days: string;
-      month: string;
-      year: string;
-      timezone: string;
-    }
-  | { message: string } {
-  if (isValidTimeZone(tz.zone)) {
-    const dt = new Date(
-      new Date().toLocaleString("en-US", { timeZone: tz.zone })
-    );
+function date(req: Request, res: Response) {
+  let tz = req.query.zone as string;
+  if (isValidTimeZone(tz)) {
+    const dt = new Date(new Date().toLocaleString("en-US", { timeZone: tz }));
     const d = dt.getDate() < 10 ? `0${dt.getDate()}` : `${dt.getDate()}`;
     const m =
       dt.getMonth() + 1 < 10 ? `0${dt.getMonth() + 1}` : `${dt.getMonth() + 1}`;
     const y = dt.getFullYear().toString();
     const t = `${d}/${m}/${y}`;
-
-    return {
-      date: t,
-      days: d,
-      month: m,
-      year: y,
-      timezone: tz.zone,
-    };
+    res.status(200).json({ date: t, days: d, month: m, year: y, timezone: tz });
   }
-  return { message: "Invalid timezone" };
+  res.status(400).json({ message: "Invalid timezone" });
 }
 
 export { time, date };
--- functions/fun.ts
+++ functions/fun.ts
@@ -1,24 +1,54 @@
-function bully(): { message: string } {
-  return { message: "Bully String" };
+import fs from "fs";
+import path from "path";
+import { Request, Response } from "express";
+
+function readFile(fileName: string) {
+  const filePath = path.join(__dirname, `../data/${fileName}`);
+  try {
+    const data = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(data);
+  } catch (e: any) {
+    console.error("Error reading facts.json : ", e.message);
+    return [];
+  }
 }
 
-function quote(): { message: string } {
-  return { message: "Quote String" };
+function bully(req: Request, res: Response) {
+  const data = readFile("bully.json");
+  const random = data[Math.floor(Math.random() * data.length)];
+  if (random) res.status(200).json({ bully: random });
+  else res.status(500).json({ bully: "Internal Server Error" });
 }
 
-function joke(): { message: string } {
-  return { message: "Joke String" };
+function quote(req: Request, res: Response) {
+  const data = readFile("quotes.json");
+  const random = data[Math.floor(Math.random() * data.length)];
+  if (random) res.status(200).json({ quote: random });
+  else res.status(500).json({ quote: "Internal Server Error" });
 }
 
-function fact(): { message: string } {
-  return { message: "Fact String" };
+function joke(req: Request, res: Response) {
+  const data = readFile("jokes.json");
+  const random = data[Math.floor(Math.random() * data.length)];
+  if (random) res.status(200).json({ joke: random });
+  else res.status(500).json({ joke: "Internal Server Error" });
 }
 
-function advice(): { message: string } {
-  return { message: "Advice String" };
+function fact(req: Request, res: Response) {
+  const data = readFile("facts.json");
+  const random = data[Math.floor(Math.random() * data.length)];
+  if (random) res.status(200).json({ fact: random });
+  else res.status(500).json({ fact: "Internal Server Error" });
 }
 
-function greet(name: string | undefined): { message: string } {
+function advice(req: Request, res: Response) {
+  const advice = readFile("advice.json");
+  const random = advice[Math.floor(Math.random() * advice.length)];
+  if (random) res.status(200).json({ advice: random });
+  else res.status(500).json({ advice: "Internal Server Error" });
+}
+
+function greet(name: string | undefined) {
   if (typeof name === "string") return { message: `Hello ${name}!` };
   return { message: "Hello User!" };
 }
