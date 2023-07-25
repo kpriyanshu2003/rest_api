51839d0972be24d8f0fad61e6653c958681c51b6
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-07-25 13:19:49 +0530

    Implemented all changes

--- app.js
+++ app.js
@@ -4,18 +4,68 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+const morgan = require("morgan");
+
+const checkAuth = (req, res, next) => {
+  if (req.path === "/profile" && req.query.auth) {
+    const filePath = path.join(__dirname, "logs", "users.json");
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading the file:", err);
+        res.status(500).end("Error reading the file");
+      }
+      const jsonData = JSON.parse(data);
+      if (jsonData.includes(req.query.auth)) console.log("Token Confirmed");
+      else {
+        console.error("Unauthorised User tried to access " + req.url);
+        res.status(401).json({ error: "Unauthorisd Access" });
+      }
+      next();
+    });
+  } else next();
+};
+
+const setCustomHeader = (req, res, next) => {
+  res.removeHeader("x-powered-by");
+  next();
+};
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "access.log"),
+  {
+    flags: "a",
+  }
+);
+const customFormat =
+  ':url :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :response-time ms - :res[content-length] ":referrer" ":user-agent"';
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "static")));
+app.use(morgan(customFormat, { stream: accessLogStream }));
+app.use(setCustomHeader);
+app.use(checkAuth);
 
+app.use(express.static(path.join(__dirname, "static")));
 const server = require("./server");
 app.use("/", server);
 
 app.use(function (req, res) {
-  res.status(404).end("error");
+  res.status(404).sendFile(path.join(__dirname, "static", "404.html"));
 });
 
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.DATABASE_URL)
+//   .then(() =>
+//     app.listen(port, () => {
+//       console.log(`Database Connected and server is running on port ${port}`);
+//     })
+//   )
+//   .catch((err) => console.log(err));
+
 app.listen(port, () => {
-  console.log("Server running at http://localhost:" + port);
+  console.log(`Server running at http://localhost:${port}`);
 });
--- server.js
+++ server.js
@@ -1,48 +1,35 @@
 // This file handles all endpoints
-
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 
-const { time, date } = require("./src/datentime");
+const { date, time } = require("./src/datentime");
 const { bully, quote, joke, fact, advice, greet } = require("./src/fun");
 const { convert } = require("./src/convert");
 const { test } = require("./src/test");
+const { auth, tool, profile } = require("./src/question");
+
+const { getAllUser, signup } = require("./controllers/user-controller"); // test - DB
+router.get("/userData", getAllUser); //test - DB
+router.post("/addUser", signup); // test - DB
+
+router.get("/auth", auth);
+router.get("/tool", tool);
+router.get("/profile", profile);
 
 router.get("/convert", (req, res) => {
   res.send(convert(req.query));
 });
 
-router.get("/time", (req, res) => {
-  res.send(time(req.query));
-});
-
-router.get("/date", (req, res) => {
-  res.send(date(req.query));
-});
-
-router.get("/bully", (req, res) => {
-  res.send(bully());
-});
-
-router.get("/joke", (req, res) => {
-  res.send(joke());
-});
-
-router.get("/advice", (req, res) => {
-  res.send(advice());
-});
+router.get("/time", time);
+router.get("/date", date);
+router.get("/bully", bully);
+router.get("/joke", joke);
+router.get("/advice", advice);
+router.get("/fact", fact);
+router.get("/quote", quote);
 
 router.get("/greet", (req, res) => {
   res.send(greet());
-});
-
-router.get("/fact", (req, res) => {
-  res.send(fact());
-});
-
-router.get("/quote", (req, res) => {
-  res.send(quote());
 });
 
 router.get("/test", (req, res) => {
@@ -51,7 +38,10 @@ router.get("/test", (req, res) => {
   console.log("Query ", req.query);
   console.log("URL ", req.url);
   console.log("Params ", req.params);
-  res.send(test());
+  console.log("User IP ", req.ip);
+  console.log("User IPs ", req.ips);
+  console.log(req.path);
+  res.send(test(req.query));
 });
 
 module.exports = router;
