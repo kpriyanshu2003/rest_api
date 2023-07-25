5c393cd8b540c6c28be3019bfd6c28f0111953b8
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-07-25 17:39:11 +0530

    Moved port to env

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.use(function (req, res) {
 //   )
 //   .catch((err) => console.log(err));
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
