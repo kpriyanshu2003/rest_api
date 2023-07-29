451db7ad05694bc6088a5c86f1a7ea5a77af2159
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-07-29 16:13:33 +0530

    Removed unused code

--- src/test.js
+++ src/test.js
@@ -1,55 +1,20 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.templating = exports.test = void 0;
-function fetchWordMeaning(word) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const response = yield fetch(`http://localhost:3000/fact`);
-            if (!response.ok) {
-                throw new Error("Failed to fetch data from the API.");
-            }
-            const data = yield response.json();
-            const meaning = data.fact;
-            if (meaning) {
-                return meaning;
-            }
-            else {
-                throw new Error("No meaning found for the word.");
-            }
-        }
-        catch (error) {
-            console.error("Error fetching meaning:", error.message);
-            return null;
-        }
+function test(req, res) {
+    console.log("\n------------------");
+    console.log("Body ", req.body);
+    console.log("Query ", req.query);
+    console.log("URL ", req.url);
+    console.log("Params ", req.params);
+    console.log("User IP ", req.ip);
+    console.log("User IPs ", req.ips);
+    console.log(req.path);
+    res.json({
+        message: "Endpoint used for testing only.",
+        instruction: "Endpoint runnig in default case.",
+        statusCode: "200",
     });
-}
-function test(val) {
-    try {
-        switch (val.func) {
-            case "dictionary":
-                console.log(fetchWordMeaning("val"));
-                return fetchWordMeaning("value");
-                break;
-            default:
-                throw new Error("Error thorown");
-        }
-    }
-    catch (e) {
-        return {
-            message: "Endpoint used for testing only.",
-            instruction: "Endpoint runnig in default case.",
-            statusCode: "200",
-        };
-    }
 }
 exports.test = test;
 const data = {
@@ -72,7 +37,6 @@ const data = {
             date: "2023-07-25",
             content: "<p>This is created using a template engine known as Mustache, this is quite easy to use and looks almost like generic HTML, CSS and JS. Also, I can use it easily alongside Express easily.",
         },
-        // Add more blog posts here...
     ],
 };
 function templating(req, res) {
--- src/test.ts
+++ src/test.ts
@@ -1,42 +1,19 @@
 import { Response, Request } from "express";
 
-async function fetchWordMeaning(word: string): Promise<string | null> {
-  try {
-    const response = await fetch(`http://localhost:3000/fact`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch data from the API.");
-    }
-    const data = await response.json();
-    const meaning = data.fact;
-    if (meaning) {
-      return meaning;
-    } else {
-      throw new Error("No meaning found for the word.");
-    }
-  } catch (error: any) {
-    console.error("Error fetching meaning:", error.message);
-    return null;
-  }
-}
-
-type Test = { func: string };
-function test(val: Test) {
-  try {
-    switch (val.func) {
-      case "dictionary":
-        console.log(fetchWordMeaning("val"));
-        return fetchWordMeaning("value");
-        break;
-      default:
-        throw new Error("Error thorown");
-    }
-  } catch (e) {
-    return {
-      message: "Endpoint used for testing only.",
-      instruction: "Endpoint runnig in default case.",
-      statusCode: "200",
-    };
-  }
+function test(req: Request, res: Response) {
+  console.log("\n------------------");
+  console.log("Body ", req.body);
+  console.log("Query ", req.query);
+  console.log("URL ", req.url);
+  console.log("Params ", req.params);
+  console.log("User IP ", req.ip);
+  console.log("User IPs ", req.ips);
+  console.log(req.path);
+  res.json({
+    message: "Endpoint used for testing only.",
+    instruction: "Endpoint runnig in default case.",
+    statusCode: "200",
+  });
 }
 
 const data = {
@@ -60,7 +37,6 @@ const data = {
       content:
         "<p>This is created using a template engine known as Mustache, this is quite easy to use and looks almost like generic HTML, CSS and JS. Also, I can use it easily alongside Express easily.",
     },
-    // Add more blog posts here...
   ],
 };
 
